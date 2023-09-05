6852a3b844e4b56dbf2fe2f780207a0da47d1f21
Ammar-Asim-23 <ammarasim065@gmail.com>
2023-09-05 18:13:33 +0500

    feat: Done Sentiment Analysis on latest headlines

--- scraper.py
+++ scraper.py
@@ -6,12 +6,13 @@ from praw.models import MoreComments
 import datetime
 import time
 
-
 reddit = praw.Reddit(client_id = client_id,
                     client_secret = client_id_secret,
                     user_agent = user_agent)
 
-subreddit = reddit.subreddit("PakCricket")
+subreddit = reddit.subreddit("politics")
+
+
 
 def get_subreddit_info(subreddit):
     # Display the name of the Subreddit
@@ -23,62 +24,29 @@ def get_subreddit_info(subreddit):
     # Display the description of the Subreddit
     print("Description:", subreddit.description)
 
-def getPosts(subreddit, time_filter, limit):
-    print("Getting posts...")
+
+
+
+def getHeadlines(subreddit):
+    print("Getting headlines...")
     startTime = time.time()
-    posts_data = {"ID" : [], "url" : [], "Title" : [], "Total Comments" : [], "Score" : []}
-    # Collecting data from the past week
-    for post in subreddit.top(time_filter = time_filter, limit = limit):
-        posts_data["ID"].append(post.id)
-        posts_data["url"].append(post.url)
-        posts_data["Title"].append(post.title)
-        posts_data["Total Comments"].append(post.num_comments)
-        posts_data["Score"].append(post.score)
+    # Collecting latest headlines
+    headlines = set()
+    for submission in reddit.subreddit('politics').new(limit=None):
+        headlines.add(submission.title)
     # print(posts_data)
     endTime = time.time()
     elapsedTime = endTime - startTime
-    return posts_data, elapsedTime
+    return headlines, elapsedTime
 
 
-def getComments(posts):
-    print("Getting comments...")
-    startTime = time.time()
-    comments = {"Post ID" : [], "Title" : [], "Date" : [], "Comment" : [], "Length" : []}
-    postIDs = posts["ID"]
-    for i in range(len(postIDs)):
-        submission = reddit.submission(id = postIDs[i])
-        for commentInstance in submission.comments[1:]:
-            if isinstance(commentInstance, MoreComments):
-                continue
-            id =  postIDs[i]
-            date = datetime.datetime.utcfromtimestamp(submission.created_utc)
-            title = submission.title
-            comments["Post ID"].append(id)
-            comments["Title"].append(title)
-            comments["Date"].append(date)
-            comments["Comment"].append(commentInstance.body)
-            comments["Length"].append(len(commentInstance.body))
-    endTime = time.time()
-    timeElapsed = endTime - startTime
 
-
-    return comments, timeElapsed
-
-current_datetime = datetime.date.today()
-str_current_datetime = current_datetime.strftime('%m/%d/%Y')
-
-pastWeekPosts, timeTaken = getPosts(subreddit, "month", 1000)
-posts_df = pd.DataFrame(pastWeekPosts)
+get_subreddit_info(subreddit)
+pastWeekPosts, timeTaken = getHeadlines(subreddit)
+headlines_df = pd.DataFrame(pastWeekPosts)
 print("Done")
 print("Time taken: ", timeTaken)
-print("Number of Posts collected: ", len(posts_df.index))
-file = "redditData/post.csv"
-posts_df.to_csv(file, index = False)
+print("Number of Headlines collected: ", len(headlines_df.index))
+file = "redditData/headlines.csv"
+headlines_df.to_csv(file, index = False)
 
-pastWeekComments, timeTaken = getComments(pastWeekPosts)
-comments_df = pd.DataFrame(pastWeekComments)
-print("Done")
-print("Time taken: ", timeTaken)
-print("Number of Comments collected: ", len(comments_df.index))
-file = "redditData/comment.csv"
-comments_df.to_csv(file, index = False)
